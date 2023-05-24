b5396cd80b176bd91f0c0aa7d9ca1a331615821f
Federico Caballero <98574058+fede-caballero@users.noreply.github.com>
2023-05-24 10:02:20 -0300

    Update Card.js

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,13 +1,22 @@
-import React from "react";
-import  Img from "./images/Lomo_sand.jpg";
-import "./Card-styles.css"
-export const Card = () => {
-    return(
-      <div class="Card" style="width: 50px;">
-      <img src={Img} class="Card-img-top" alt="..."/>
-      <div class="Card-body">
-        <p class="Card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-      </div>
+import React from 'react'
+import IPA from './images/IPA.png'
+
+export const Card = (props) => {
+  return (
+<div>
+    <div className= "row">
+      <div className='col'>
+        <div className="card" style={{width: '18rem', backgroundColor: 'rgba(50, 200, 0, 0.1)' }}>
+          <img src={require(`../components/images/${props.imagen}`)} className="card-img-top" alt=".." />
+            <div className="card-body">
+              <h1 className='titulo'>{ props.titulo }</h1>
+              <p className="texto">{ props.texto }</p>
+              <a href="#" class="btn btn-primary">+Info</a>
+            </div>
+        </div>
     </div>
-    )
+  </div>
+</div>
+  )
 }
+
